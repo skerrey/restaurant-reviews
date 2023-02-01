7fa3513448c3f17990a0d14f1599bf5382aee3b6
skerrey <sethkerrey@gmail.com>
2023-01-31 18:42:03 -0600

    Update reviewsDAO.js

---
+++ backend/dao/reviewsDAO.js
@@ -0,0 +1,65 @@
+// Description: Data Access Objects for reviews in restaurant DB
+
+import mongodb from "mongodb";
+
+const ObjectId = mongodb.ObjectId; // convert string to object ID
+
+let reviews;
+
+export default class ReviewsDAO { 
+  static async injectDB(conn) { // See if reviews exist
+    if (reviews) {
+      return
+    }
+    try {
+      reviews = await conn.db(process.env.RESTREVIEW_NS).collection("reviews");
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`);
+    }
+  }
+
+  static async addReview(restaurantId, user, review, date) { // Add review to DB
+    try {
+      const reviewDoc = { 
+        name: user.name,
+        date: date,
+        text: review,
+        restaurant_id: ObjectId(restaurantId),
+      };
+
+      return await reviews.insertOne(reviewDoc); // insert into DB 
+    } catch (e) { 
+      console.error(`Unable to post review: ${e}`);
+      return { error: e };
+    }
+  }
+
+  static async updateReview(reviewId, userId, text, date) { // Updates review in DB
+    try {
+      const updateResponse = await reviews.updateOne(
+        { user_id: userId, _id: ObjectId(reviewId)}, // look for review with correct ID's
+        { $set: { text: text, date: date } }, // update text and date
+      )
+
+      return updateResponse;
+    } catch (e) {
+      console.error(`Unable to update review: ${e}`);
+      return { error: e };
+    }
+  }
+
+  static async deleteReview(reviewId, userId) { // Deletes review from DB
+    try {
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId(reviewId),
+        user_id: userId,
+      });
+
+      return deleteResponse;
+    } catch (e) {
+      console.error(`Unable to delete review: ${e}`);
+      return { error: e };
+    }
+  }
+
+}
