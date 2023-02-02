61e45fe0ce12e3e1978695f60a31fadbb42829a3
skerrey <sethkerrey@gmail.com>
2023-02-02 14:49:43 -0600

    Comments

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -6,6 +6,7 @@ import ReviewsCtrl from "./reviews.controller.js";
 
 const router = express.Router(); // Router for routes
 
+// Routes
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
 router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById);
 router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines);
