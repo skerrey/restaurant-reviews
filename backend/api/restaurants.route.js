712a3f65b81b696ca23debafdd3259e65621a868
skerrey <sethkerrey@gmail.com>
2023-02-01 19:05:12 -0600

    Update restaurants.route.js

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -7,9 +7,11 @@ import ReviewsCtrl from "./reviews.controller.js";
 const router = express.Router(); // Router for routes
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById);
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines);
 
 router
-  .route("review")
+  .route("/review")
   .post(ReviewsCtrl.apiPostReview)
   .put(ReviewsCtrl.apiUpdateReview)
   .delete(ReviewsCtrl.apiDeleteReview);
