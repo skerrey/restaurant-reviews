d1ef7e937546dc428a05e197c5312c23ddf104af
skerrey <sethkerrey@gmail.com>
2023-02-01 19:05:23 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
 import RestaurantsDAO from "./dao/restaurantsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ MongoClient.connect(
   })
   .then(async client => {
     await RestaurantsDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     })
