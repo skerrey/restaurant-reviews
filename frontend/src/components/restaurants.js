d5792ad8698a40dc61512bb49dd08a3ffc82650e
skerrey <sethkerrey@gmail.com>
2023-02-04 12:34:30 -0600

    Update restaurants.js

---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,13 @@
+// Description: Component to get one restaurant
+
+import React from "react";
+
+function Restaurant() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default Restaurant;
