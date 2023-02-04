e9c7a1e447af490653565291b5f180396d6aa419
skerrey <sethkerrey@gmail.com>
2023-02-04 12:34:23 -0600

    Update add-review.js

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,13 @@
+// Description: Component to add review
+
+import React from "react";
+
+function AddReview() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default AddReview;
