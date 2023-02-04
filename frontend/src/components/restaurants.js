9fb653ea7312e213833141eeb7fb62b72c12313e
skerrey <sethkerrey@gmail.com>
2023-02-04 13:12:16 -0600

    Update restaurants.js

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,13 +1,107 @@
-// Description: Component to get one restaurant
+// Description: Component to get one restaurant and its reviews
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-function Restaurant() {
-  return (
-    <div className="App">
-      Hello World
+const Restaurant = props => {
+  const initialRestaurantState = { // create empty restaurant state
+    id: null,
+    name: "",
+    address: {},
+    cuisine: "",
+    reviews: []
+  };
+  const [restaurant, setRestaurant] = useState(initialRestaurantState);
+
+  const getRestaurant = id => { // get restaurant from database
+    RestaurantDataService.get(id)
+      .then(res => {
+        setRestaurant(res.data);
+        console.log(res.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => { // use only when id changes
+    getRestaurant(props.match.params.id);
+  }, [props.match.params.id]);
+
+  const deleteReview = (reviewId, index) => { // delete review from database
+    RestaurantDataService.deleteReview(reviewId, props.user.id)
+      .then(res => {
+        setRestaurant((prevState) => {
+          prevState.reviews.splice(index, 1)
+          return({
+            ...prevState
+          })
+        })
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  return ( // display restaurant info
+    <div>
+      {restaurant ? (
+        <div>
+          <h5>{restaurant.name}</h5>
+          <p>
+            <strong>Cuisine: </strong>{restaurant.cuisine}<br/>
+            <strong>Address: </strong>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}
+          </p>
+          <Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-primary">
+            Add Review
+          </Link>
+          <h4> Reviews </h4>
+          <div className="row">
+            {restaurant.reviews.length > 0 ? ( 
+             restaurant.reviews.map((review, index) => {
+               return (
+                 <div className="col-lg-4 pb-1" key={index}>
+                   <div className="card">
+                     <div className="card-body">
+                       <p className="card-text">
+                         {review.text}<br/>
+                         <strong>User: </strong>{review.name}<br/>
+                         <strong>Date: </strong>{review.date}
+                       </p>
+                       {props.user && props.user.id === review.user_id &&
+                          <div className="row">
+                            <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
+                            <Link to={{
+                              pathname: "/restaurants/" + props.match.params.id + "/review",
+                              state: {
+                                currentReview: review
+                              }
+                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
+                          </div>                   
+                       }
+                     </div>
+                   </div>
+                 </div>
+               );
+             })
+            ) : (
+            <div className="col-sm-4">
+              <p>No reviews yet.</p>
+            </div>
+            )}
+
+          </div>
+
+        </div>
+      ) : (
+        <div>
+          <br />
+          <p>No restaurant selected.</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Restaurant;
