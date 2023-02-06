6e72ca9a143b81ebcebc82d0de6d75a8df4b4e2d
skerrey <sethkerrey@gmail.com>
2023-02-05 21:53:34 -0600

    Update helper.js

--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,4 +1,4 @@
 // Description: Provides base url for backend
 
 // export const BASE_URL = "http://localhost:5000"
-export const BASE_URL = "https://mernappbackend.onrender.com"
+export const BASE_URL = "https://restaurant-reviews-backend-d75s.onrender.com"
