0604cd6050073339bdc9d74be0ae86f94d15cd99
skerrey <sethkerrey@gmail.com>
2023-02-05 21:52:45 -0600

    Add BASE_URL

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -1,34 +1,35 @@
 // Description: Create API calls 
 
 import http from "../http-common";
+import { BASE_URL } from "./helper";
 
 class RestaurantDataService {
   getAll(page = 0) { // Default page is 0
-    return http.get(`?page=${page}`);
+    return http.get(`${BASE_URL}/?page=${page}`);
   }
 
   get(id) {
-    return http.get(`/id/${id}`);
+    return http.get(`${BASE_URL}/id/${id}`);
   }
 
   find(query, by = "name", page = 0) {
-    return http.get(`?${by}=${query}&page=${page}`);
+    return http.get(`${BASE_URL}/?${by}=${query}&page=${page}`);
   }
 
   createReview(data) {
-    return http.post("/review", data);
+    return http.post(`${BASE_URL}/review`, data);
   }
 
   updateReview(data) {
-    return http.put("/review", data);
+    return http.put(`${BASE_URL}/review`, data);
   }
 
   deleteReview(id, userId) {
-    return http.delete(`/review?id=${id}`, { data: { user_id: userId } });
+    return http.delete(`${BASE_URL}/review?id=${id}`, { data: { user_id: userId } });
   }
 
   getCuisines(id) {
-    return http.get(`/cuisines`);
+    return http.get(`${BASE_URL}/cuisines`);
   }
 
 }
