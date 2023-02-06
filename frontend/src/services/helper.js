6e9cd62c3c3e5b34e485de893c56ed0ead1ee510
skerrey <sethkerrey@gmail.com>
2023-02-05 22:04:50 -0600

    update

--- frontend/src/services/helper.js
+++
@@ -1,4 +0,0 @@
-// Description: Provides base url for backend
-
-// export const BASE_URL = "http://localhost:5000"
-export const BASE_URL = "https://restaurant-reviews-backend-d75s.onrender.com"
--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -1,35 +1,34 @@
 // Description: Create API calls 
 
 import http from "../http-common";
-import { BASE_URL } from "./helper";
 
 class RestaurantDataService {
   getAll(page = 0) { // Default page is 0
-    return http.get(`${BASE_URL}/?page=${page}`);
+    return http.get(`?page=${page}`);
   }
 
   get(id) {
-    return http.get(`${BASE_URL}/id/${id}`);
+    return http.get(`/id/${id}`);
   }
 
   find(query, by = "name", page = 0) {
-    return http.get(`${BASE_URL}/?${by}=${query}&page=${page}`);
+    return http.get(`?${by}=${query}&page=${page}`);
   }
 
   createReview(data) {
-    return http.post(`${BASE_URL}/review`, data);
+    return http.post("/review", data);
   }
 
   updateReview(data) {
-    return http.put(`${BASE_URL}/review`, data);
+    return http.put("/review", data);
   }
 
   deleteReview(id, userId) {
-    return http.delete(`${BASE_URL}/review?id=${id}`, { data: { user_id: userId } });
+    return http.delete(`/review?id=${id}`, { data: { user_id: userId } });
   }
 
   getCuisines(id) {
-    return http.get(`${BASE_URL}/cuisines`);
+    return http.get(`/cuisines`);
   }
 
 }
