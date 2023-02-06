dbc613cdceb98bb2a57efe7dbc3b648700f3e6c7
skerrey <sethkerrey@gmail.com>
2023-02-05 22:41:19 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express(); // Initialize express
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Enable express to parse JSON
 
-app.use("https://restaurant-reviews-backend-h23u.onrender.com/api/v1/restaurants", restaurants); // Use restaurants route
+app.use("/api/v1/restaurants", restaurants); // Use restaurants route
 app.use("*", (req, res) => res.status(404).json({ error: "Page not found" })); // Wildcard 404 route
 
 export default app;
