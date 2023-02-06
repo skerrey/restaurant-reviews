d3cde0e0a3c31a3513d0b617a78617f048b80a74
skerrey <sethkerrey@gmail.com>
2023-02-05 19:08:08 -0600

    Add iframe, icons, styling

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,8 +1,16 @@
 // Description: Component to get one restaurant and its reviews
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense, lazy } from "react";
 import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
+
+import userImg  from "../img/userImg.png" // image by Smashicons from Flaticon
+
+// import icons
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+const IFrame = lazy(() => import("./iframe"));
 
 const Restaurant = props => {
   const initialRestaurantState = { // create empty restaurant state
@@ -45,39 +53,75 @@ const Restaurant = props => {
   };
 
   return ( // display restaurant info
-    <div>
+    <div style={{
+      height: "100vh"
+    }}>
       {restaurant ? (
         <div>
-          <h5>{restaurant.name}</h5>
-          <p>
-            <strong>Cuisine: </strong>{restaurant.cuisine}<br/>
-            <strong>Address: </strong>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}
-          </p>
-          <Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-primary">
+          <div className="col-lg-8 bg-white rounded px-2">
+            <h3>{restaurant.name}</h3>
+            <p>
+              <strong>Cuisine: </strong>{restaurant.cuisine}<br/>
+              <strong>Address: </strong>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}
+            </p>
+
+            <div>
+              {/* The lazy component should then be rendered inside a Suspense component, which allows us to show some fallback
+              content (such as a loading indicator) while we’re waiting for the lazy component to load */}
+              {/* The fallback prop accepts any React elements that you want to render while waiting for the component to load */}
+              <Suspense fallback={<div>Loading...</div>}>
+                <IFrame src={`https://www.google.com/maps?q=${restaurant.address.building}${restaurant.address.street}${restaurant.address.zipcode}&output=embed`} width="100%" height="500px" />
+              </Suspense>
+            </div>
+
+          </div>
+
+          <Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-warning mt-2">
             Add Review
           </Link>
-          <h4> Reviews </h4>
+          <h4 className="pt-1">Reviews</h4>
           <div className="row">
             {restaurant.reviews.length > 0 ? ( 
              restaurant.reviews.map((review, index) => {
                return (
+                
                  <div className="col-lg-4 pb-1" key={index}>
                    <div className="card">
-                     <div className="card-body">
-                       <p className="card-text">
-                         {review.text}<br/>
-                         <strong>User: </strong>{review.name}<br/>
-                         <strong>Date: </strong>{review.date}
-                       </p>
-                       {props.user && props.user.id === review.user_id &&
+                      <div className="card-header d-flex align-items-end">
+                        <div>
+                          <img src={userImg} alt="user" /><br />
+                        </div>
+                        <div className="h3">
+                          &nbsp; {review.name}
+                        </div>
+                        <div className="ms-auto">
+                          <em>{review.date}</em>
+                        </div>
+                      </div>
+                      <div className="card-body">
+                        <p className="card-text">
+                            "{review.text}"
+                        </p>
+                        {props.user && props.user.id === review.user_id &&
                           <div className="row">
-                            <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
+                            <button onClick={() => deleteReview(review._id, index)} className="btn btn-danger col-lg-5 mx-1 mb-1">
+                              <div className="d-flex justify-content-center align-items-center">
+                                <div>Delete &nbsp;</div>
+                                <RiDeleteBin6Line />
+                              </div>
+                            </button>
                             <Link to={{
                               pathname: "/restaurants/" + props.match.params.id + "/review",
                               state: {
                                 currentReview: review
                               }
-                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
+                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">
+                              <div className="d-flex justify-content-center align-items-center">
+                                <div>Edit &nbsp;</div>
+                                <FaEdit />
+                              </div>
+
+                            </Link>
                           </div>                   
                        }
                      </div>
